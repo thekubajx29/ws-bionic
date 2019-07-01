d6eea5d07b4de5d770594c5d2858ae3be1f3bfe7
Matthew Wilcox (Oracle) <willy@infradead.org>
2019-07-01 17:03:29 -0400

    XArray: Fix xas_next() with a single entry at 0
    BugLink: https://bugs.launchpad.net/bugs/1856213
    
    [ Upstream commit 91abab83839aa2eba073e4a63c729832fdb27ea1 ]
    
    If there is only a single entry at 0, the first time we call xas_next(),
    we return the entry.  Unfortunately, all subsequent times we call
    xas_next(), we also return the entry at 0 instead of noticing that the
    xa_index is now greater than zero.  This broke find_get_pages_contig().
    
    Fixes: 64d3e9a9e0cc ("xarray: Step through an XArray")
    Reported-by: Kent Overstreet <kent.overstreet@gmail.com>
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- lib/xarray.c
+++ lib/xarray.c
@@ -983,6 +983,8 @@ void *__xas_prev(struct xa_state *xas)
 
 	if (!xas_frozen(xas->xa_node))
 		xas->xa_index--;
+	if (!xas->xa_node)
+		return set_bounds(xas);
 	if (xas_not_node(xas->xa_node))
 		return xas_load(xas);
 
@@ -1020,6 +1022,8 @@ void *__xas_next(struct xa_state *xas)
 
 	if (!xas_frozen(xas->xa_node))
 		xas->xa_index++;
+	if (!xas->xa_node)
+		return set_bounds(xas);
 	if (xas_not_node(xas->xa_node))
 		return xas_load(xas);
 
