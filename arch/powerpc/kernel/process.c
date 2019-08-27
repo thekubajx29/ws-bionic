25f7a96d3d798a4a245d02e4625826ff0320e286
Nicholas Piggin <npiggin@gmail.com>
2019-08-27 13:30:06 +1000

    powerpc: convert to copy_thread_tls
    BugLink: https://bugs.launchpad.net/bugs/1862059
    
    Commit 3033f14ab78c3 ("clone: support passing tls argument via C rather
    than pt_regs magic") introduced the HAVE_COPY_THREAD_TLS option. Use it
    to avoid a subtle assumption about the argument ordering of clone type
    syscalls.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20190827033010.28090-2-npiggin@gmail.com
    (cherry picked from commit facd04a904ff6cdc6ee85d6e85d500f478a1bec4)
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- arch/powerpc/kernel/process.c
+++ arch/powerpc/kernel/process.c
@@ -1612,8 +1612,9 @@ static void setup_ksp_vsid(struct task_struct *p, unsigned long sp)
 /*
  * Copy architecture-specific thread state
  */
-int copy_thread(unsigned long clone_flags, unsigned long usp,
-		unsigned long kthread_arg, struct task_struct *p)
+int copy_thread_tls(unsigned long clone_flags, unsigned long usp,
+		unsigned long kthread_arg, struct task_struct *p,
+		unsigned long tls)
 {
 	struct pt_regs *childregs, *kregs;
 	extern void ret_from_fork(void);
@@ -1654,10 +1655,10 @@ int copy_thread(unsigned long clone_flags, unsigned long usp,
 		if (clone_flags & CLONE_SETTLS) {
 #ifdef CONFIG_PPC64
 			if (!is_32bit_task())
-				childregs->gpr[13] = childregs->gpr[6];
+				childregs->gpr[13] = tls;
 			else
 #endif
-				childregs->gpr[2] = childregs->gpr[6];
+				childregs->gpr[2] = tls;
 		}
 
 		f = ret_from_fork;
