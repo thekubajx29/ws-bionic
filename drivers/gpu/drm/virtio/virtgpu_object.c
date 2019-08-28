6455917646fbf7946a568817b24a6ff85c76fa68
Stephen Rothwell <sfr@canb.auug.org.au>
2019-08-28 18:55:16 +1000

    drm/virtio: module_param_named() requires linux/moduleparam.h
    BugLink: https://bugs.launchpad.net/bugs/1869413
    
    commit b0138364da17617db052c4a738b58bf45e42f500 upstream.
    
    Fixes: 3e93bc2a58aa ("drm/virtio: make resource id workaround runtime switchable.")
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Link: http://patchwork.freedesktop.org/patch/msgid/20190828185516.22b03da8@canb.auug.org.au
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/gpu/drm/virtio/virtgpu_object.c
+++ drivers/gpu/drm/virtio/virtgpu_object.c
@@ -23,6 +23,8 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include <linux/moduleparam.h>
+
 #include "virtgpu_drv.h"
 
 static int virtio_gpu_virglrenderer_workaround = 1;
