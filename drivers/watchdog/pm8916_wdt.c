421dc66a0e2d3730a4350040a8aa6f84da1ca07e
Jorge Ramirez-Ortiz <jorge.ramirez-ortiz@linaro.org>
2019-09-06 22:30:53 +0200

    watchdog: pm8916_wdt: fix pretimeout registration flow
    BugLink: https://bugs.launchpad.net/bugs/1856213
    
    [ Upstream commit 1993f1d7ca3f315e0459c58c8e7038039a96dd85 ]
    
    When an IRQ is present in the dts, the probe function shall fail if
    the interrupt can not be registered.
    
    The probe function shall also be retried if getting the irq is being
    deferred.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez-ortiz@linaro.org>
    Reviewed-by: Loic Poulain <loic.poulain@linaro.org>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Wim Van Sebroeck <wim@linux-watchdog.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/watchdog/pm8916_wdt.c
+++ drivers/watchdog/pm8916_wdt.c
@@ -162,9 +162,17 @@ static int pm8916_wdt_probe(struct platform_device *pdev)
 
 	irq = platform_get_irq(pdev, 0);
 	if (irq > 0) {
-		if (devm_request_irq(&pdev->dev, irq, pm8916_wdt_isr, 0,
-				     "pm8916_wdt", wdt))
-			irq = 0;
+		err = devm_request_irq(&pdev->dev, irq, pm8916_wdt_isr, 0,
+				       "pm8916_wdt", wdt);
+		if (err)
+			return err;
+
+		wdt->wdev.info = &pm8916_wdt_pt_ident;
+	} else {
+		if (irq == -EPROBE_DEFER)
+			return -EPROBE_DEFER;
+
+		wdt->wdev.info = &pm8916_wdt_ident;
 	}
 
 	/* Configure watchdog to hard-reset mode */
@@ -176,7 +184,6 @@ static int pm8916_wdt_probe(struct platform_device *pdev)
 		return err;
 	}
 
-	wdt->wdev.info = (irq > 0) ? &pm8916_wdt_pt_ident : &pm8916_wdt_ident,
 	wdt->wdev.ops = &pm8916_wdt_ops,
 	wdt->wdev.parent = &pdev->dev;
 	wdt->wdev.min_timeout = PM8916_WDT_MIN_TIMEOUT;
