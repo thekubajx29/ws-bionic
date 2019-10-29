96a076b644e1c7ba4a12b0dc2fc17c6c17e982e4
Björn Töpel <bjorn.topel@intel.com>
2019-10-29 16:43:07 +0100

    bpf: Change size to u64 for bpf_map_{area_alloc, charge_init}()
    BugLink: https://bugs.launchpad.net/bugs/1856213
    
    [ Upstream commit ff1c08e1f74b6864854c39be48aa799a6a2e4d2b ]
    
    The functions bpf_map_area_alloc() and bpf_map_charge_init() prior
    this commit passed the size parameter as size_t. In this commit this
    is changed to u64.
    
    All users of these functions avoid size_t overflows on 32-bit systems,
    by explicitly using u64 when calculating the allocation size and
    memory charge cost. However, since the result was narrowed by the
    size_t when passing size and cost to the functions, the overflow
    handling was in vain.
    
    Instead of changing all call sites to size_t and handle overflow at
    the call site, the parameter is changed to u64 and checked in the
    functions above.
    
    Fixes: d407bd25a204 ("bpf: don't trigger OOM killer under pressure with map alloc")
    Fixes: c85d69135a91 ("bpf: move memory size checks to bpf_map_charge_init()")
    Signed-off-by: Björn Töpel <bjorn.topel@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Reviewed-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Link: https://lore.kernel.org/bpf/20191029154307.23053-1-bjorn.topel@gmail.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -134,7 +134,7 @@ static struct bpf_map *find_and_alloc_map(union bpf_attr *attr)
 	return map;
 }
 
-void *bpf_map_area_alloc(size_t size, int numa_node)
+void *bpf_map_area_alloc(u64 size, int numa_node)
 {
 	/* We definitely need __GFP_NORETRY, so OOM killer doesn't
 	 * trigger under memory pressure as we really just want to
@@ -142,6 +142,9 @@ void *bpf_map_area_alloc(size_t size, int numa_node)
 	 */
 	const gfp_t flags = __GFP_NOWARN | __GFP_NORETRY | __GFP_ZERO;
 	void *area;
+
+	if (size >= SIZE_MAX)
+		return NULL;
 
 	if (size <= (PAGE_SIZE << PAGE_ALLOC_COSTLY_ORDER)) {
 		area = kmalloc_node(size, GFP_USER | flags, numa_node);
