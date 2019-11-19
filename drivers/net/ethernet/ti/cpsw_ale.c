a3f942adc6e9d1b67a81f70b8223789316cf29bd
Grygorii Strashko <grygorii.strashko@ti.com>
2019-11-20 00:19:13 +0200

    net: ethernet: ti: ale: clean ale tbl on init and intf restart
    BugLink: https://bugs.launchpad.net/bugs/1860014
    
    [ Upstream commit 7fe579dfb90fcdf0c7722f33c772d5f0d1bc7cb6 ]
    
    Clean CPSW ALE on init and intf restart (up/down) to avoid reading obsolete
    or garbage entries from ALE table.
    
    Signed-off-by: Grygorii Strashko <grygorii.strashko@ti.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/net/ethernet/ti/cpsw_ale.c
+++ drivers/net/ethernet/ti/cpsw_ale.c
@@ -794,6 +794,7 @@ EXPORT_SYMBOL_GPL(cpsw_ale_start);
 void cpsw_ale_stop(struct cpsw_ale *ale)
 {
 	del_timer_sync(&ale->timer);
+	cpsw_ale_control_set(ale, 0, ALE_CLEAR, 1);
 	cpsw_ale_control_set(ale, 0, ALE_ENABLE, 0);
 }
 EXPORT_SYMBOL_GPL(cpsw_ale_stop);
@@ -878,6 +879,7 @@ struct cpsw_ale *cpsw_ale_create(struct cpsw_ale_params *params)
 					ALE_UNKNOWNVLAN_FORCE_UNTAG_EGRESS;
 	}
 
+	cpsw_ale_control_set(ale, 0, ALE_CLEAR, 1);
 	return ale;
 }
 EXPORT_SYMBOL_GPL(cpsw_ale_create);
