0315f1241abd855d5d6a576039ecf0bb5bf725a0
Zhang Rui <rui.zhang@intel.com>
2019-11-25 07:56:00 +0100

    powercap/intel_rapl: add support for Cometlake desktop
    BugLink: https://bugs.launchpad.net/bugs/1847454
    
    Add CometLake desktop support in intel_rapl driver
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (backported from commit f84fdcbc8ec02ea34bbc641359c2a69d0d1242d4 linux-next)
    Signed-off-by: You-Sheng Yang <vicamo.yang@canonical.com>
    Acked-by: Anthony Wong <anthony.wong@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/powercap/intel_rapl.c
+++ drivers/powercap/intel_rapl.c
@@ -1157,6 +1157,7 @@ static const struct x86_cpu_id rapl_ids[] __initconst = {
 	INTEL_CPU_FAM6(KABYLAKE_DESKTOP,	rapl_defaults_core),
 	INTEL_CPU_FAM6(CANNONLAKE_MOBILE,	rapl_defaults_core),
 	INTEL_CPU_FAM6(COMETLAKE_L,		rapl_defaults_core),
+	INTEL_CPU_FAM6(COMETLAKE,		rapl_defaults_core),
 
 	INTEL_CPU_FAM6(ATOM_SILVERMONT,		rapl_defaults_byt),
 	INTEL_CPU_FAM6(ATOM_AIRMONT,		rapl_defaults_cht),
