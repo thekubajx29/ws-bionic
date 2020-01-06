27d3db484016bb38c9171f960184c6854113abdd
Wenpeng Liang <liangwenpeng@huawei.com>
2020-01-06 20:21:10 +0800

    RDMA/hns: Avoid printing address of mtt page
    BugLink: https://bugs.launchpad.net/bugs/1868290
    
    [ Upstream commit eca44507c3e908b7362696a4d6a11d90371334c6 ]
    
    Address of a page shouldn't be printed in case of security issues.
    
    Link: https://lore.kernel.org/r/1578313276-29080-2-git-send-email-liweihang@huawei.com
    Signed-off-by: Wenpeng Liang <liangwenpeng@huawei.com>
    Signed-off-by: Weihang Li <liweihang@huawei.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/infiniband/hw/hns/hns_roce_mr.c
+++ drivers/infiniband/hw/hns/hns_roce_mr.c
@@ -1024,8 +1024,9 @@ int hns_roce_ib_umem_write_mtt(struct hns_roce_dev *hr_dev,
 				sg_dma_address(sg) + (k << umem->page_shift);
 			if (!(npage % (1 << (mtt->page_shift - PAGE_SHIFT)))) {
 				if (page_addr & ((1 << mtt->page_shift) - 1)) {
-					dev_err(dev, "page_addr 0x%llx is not page_shift %d alignment!\n",
-						page_addr, mtt->page_shift);
+					dev_err(dev,
+						"page_addr is not page_shift %d alignment!\n",
+						mtt->page_shift);
 					ret = -EINVAL;
 					goto out;
 				}
