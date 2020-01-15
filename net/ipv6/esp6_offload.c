32d42e6954156e2bb08a279bdc4d0c9224515d57
Ulrich Weber <ulrich.weber@gmail.com>
2020-01-15 12:11:29 +0100

    xfrm: support output_mark for offload ESP packets
    BugLink: https://bugs.launchpad.net/bugs/1864552
    
    commit 4e4362d2bf2a49ff44dbbc9585207977ca3d71d0 upstream.
    
    Commit 9b42c1f179a6 ("xfrm: Extend the output_mark") added output_mark
    support but missed ESP offload support.
    
    xfrm_smark_get() is not called within xfrm_input() for packets coming
    from esp4_gro_receive() or esp6_gro_receive(). Therefore call
    xfrm_smark_get() directly within these functions.
    
    Fixes: 9b42c1f179a6 ("xfrm: Extend the output_mark to support input direction and masking.")
    Signed-off-by: Ulrich Weber <ulrich.weber@gmail.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- net/ipv6/esp6_offload.c
+++ net/ipv6/esp6_offload.c
@@ -82,6 +82,8 @@ static struct sk_buff *esp6_gro_receive(struct list_head *head,
 		if (!x)
 			goto out_reset;
 
+		skb->mark = xfrm_smark_get(skb->mark, x);
+
 		sp->xvec[sp->len++] = x;
 		sp->olen++;
 
