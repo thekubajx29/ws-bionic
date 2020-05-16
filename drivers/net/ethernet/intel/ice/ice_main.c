45f66bd770b29622e77bd379209d0dfd19e614bb
Jacob Keller <jacob.e.keller@intel.com>
2020-05-15 17:42:24 -0700

    ice: fix potential double free in probe unrolling
    BugLink: https://bugs.launchpad.net/bugs/1886105
    
    [ Upstream commit bc3a024101ca497bea4c69be4054c32a5c349f1d ]
    
    If ice_init_interrupt_scheme fails, ice_probe will jump to clearing up
    the interrupts. This can lead to some static analysis tools such as the
    compiler sanitizers complaining about double free problems.
    
    Since ice_init_interrupt_scheme already unrolls internally on failure,
    there is no need to call ice_clear_interrupt_scheme when it fails. Add
    a new unroll label and use that instead.
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- drivers/net/ethernet/intel/ice/ice_main.c
+++ drivers/net/ethernet/intel/ice/ice_main.c
@@ -2124,7 +2124,7 @@ static int ice_probe(struct pci_dev *pdev,
 		dev_err(&pdev->dev,
 			"ice_init_interrupt_scheme failed: %d\n", err);
 		err = -EIO;
-		goto err_init_interrupt_unroll;
+		goto err_init_vsi_unroll;
 	}
 
 	/* Driver is mostly up */
@@ -2186,6 +2186,7 @@ err_msix_misc_unroll:
 	ice_free_irq_msix_misc(pf);
 err_init_interrupt_unroll:
 	ice_clear_interrupt_scheme(pf);
+err_init_vsi_unroll:
 	devm_kfree(&pdev->dev, pf->vsi);
 err_init_pf_unroll:
 	ice_deinit_pf(pf);
