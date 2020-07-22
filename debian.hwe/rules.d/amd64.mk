2023a724de36db4b396d80db75a26948c7772d8d
Stefan Bader <stefan.bader@canonical.com>
2020-07-22 12:47:38 +0200

    UBUNTU: [Packaging] Stop building nvidia DKMS drivers
    Since the 5.3 kernel is no longer the hwe kernel for Bionic and all
    remaining usage of its source does not use the signed nvidia drivers,
    disable building them.
    
    Ignore: yes
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- debian.hwe/rules.d/amd64.mk
+++ debian.hwe/rules.d/amd64.mk
@@ -22,6 +22,6 @@ do_extras_package = true
 do_tools_common = true
 do_tools_acpidbg = true
 do_zfs		= true
-do_dkms_nvidia  = true
+do_dkms_nvidia  = false
 do_dkms_vbox    = true
 do_dkms_wireguard = true
